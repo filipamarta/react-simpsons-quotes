7a9de0819b7b12a9464319139fa560c06bf09d0a
Filipa Marta <filipa.marta@gmail.com>
2020-03-30 12:35:50 +0100

    add Navbar.css

---
+++ src/components/Navbar.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './Navbar.css';
+
+
+function Navbar() {
+    return (
+        <nav className="Navbar">
+            <a href="https://en.wikipedia.org/wiki/The_Simpsons">Simpson Quotes</a>
+        </nav>
+    );
+}
+
+export default Navbar;
